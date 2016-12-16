822838f42dd03a3f9e0f4613c422d25c750ce9ba
Mark Frieman <mfrieman@hotmail.com>
2016-12-16 09:27:42 -0500

    first commit

---
+++ src/com/bah/utils/DateUtils.java
@@ -0,0 +1,15 @@
+package com.bah.utils;
+
+import java.util.Date;
+
+public class DateUtils {
+	public static void main(String[] args){
+		
+		System.out.println(getLocalCurrentDate());
+		
+	}
+	
+	private static Date getLocalCurrentDate(){
+		return new Date();
+	}
+}
